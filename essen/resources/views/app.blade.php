b4737e27f508a645d301b97b6662877a20794f80
Mark Lösche <mloesche@bbw-fi.de>
2022-01-31 02:13:23 +0100

    Controller und Model gemacht

--- essen/resources/views/app.blade.php
+++ essen/resources/views/app.blade.php
@@ -5,5 +5,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body><h1 class="text-center text-3xl font-bold underline"> Hello world! </h1></body>
+<body><h1 class="text-center text-3xl font-bold underline"> Essen </h1></body>
 </html>
